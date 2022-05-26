925b44c088d8f50d884399b5c4ce3a4ce4a32e6f
Leo Cornelius <56726537+leocornelius@users.noreply.github.com>
2022-05-26 11:25:23 +0100

    Create find_car.py

---
+++ find_car.py
@@ -0,0 +1,28 @@
+import cv2
+
+cap = cv2.VideoCapture('video.avi')
+car_detector = cv2.CascadeClassifier('car.xml')
+ 
+while True:
+    # Get the seperate frames
+    ret, frames = cap.read()
+     
+    # convert to gray scale of each frames
+    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)
+   
+    # Detects each car
+    cars = car_detector.detectMultiScale(gray, 1.1, 1)
+     
+    # To draw a rectangle in each cars
+    for (x,y,w,h) in cars:
+        cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2)
+ 
+   # Display frames in a window 
+   cv2.imshow('video2', frames)
+     
+    # Wait for Esc key to stop
+    if cv2.waitKey(33) == 27:
+        break
+ 
+# De-allocate any associated memory usage
+cv2.destroyAllWindows()
